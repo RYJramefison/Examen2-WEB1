5c147bfa1ca3d4a59330fcbebd004df5058061a0
RYJramefison <hei.juninho.2@gmail.com>
2024-04-25 21:48:40 +0200

    fix: script navbar

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,8 @@
-let positionPrecedente = window.pageYOffset;
+let positionPrecedente = window.scrollY;
 const navbar = document.querySelector("nav");
 
 window.addEventListener('scroll', function() {
-    let positionActuelle = window.pageYOffset;
+    let positionActuelle = window.scrollY;
     if (positionPrecedente > positionActuelle && positionActuelle === 0) {
         navbar.classList.remove("styleNavBar");
     } else {
