5b4528e04c2b551e8ac41fda674e16d0a8af1fd5
RYJramefison <hei.juninho.2@gmail.com>
2024-05-01 12:07:07 +0200

    feat: add js login

--- javascript/scriptLogin.js
+++ javascript/scriptLogin.js
@@ -1,16 +1,26 @@
-const boutton = document.getElementById("loginBoutton");
+const boutton = document.querySelector(".loginBoutton");
+
 boutton.addEventListener("click", function() {
     let email = document.querySelector(".inputEmail").value;
     let password = document.querySelector(".inputPassword").value;
 
     if (email === "test@gmail.com" && password === "motdepasse") {
-        document.getElementById("loginBoutton").style.display = "none";
-        document.getElementById("iconBoutton").style.display = "block";
+
+        boutton.innerText = ".......";
+        boutton.classList.add("addchangement")
         setTimeout(function() {
             window.location.href = "./html/contain.html";
         }, 3000);
-
     } else {
-        alert("erreur")
+        boutton.innerText = ".......";
+        boutton.classList.add("addchangement")
+        setTimeout(function() {
+            boutton.innerText = "Login Now";
+            boutton.classList.remove("addchangement")
+            document.querySelector(".erreur").style.display = "block";
+            setTimeout(function() {
+                document.querySelector(".erreur").style.display = "none";
+            }, 2000);
+        }, 3000);
 }
 });
